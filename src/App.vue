a69168415279bd4ff55f428d9f634c3c8ef6147e
Maximilian Schwarzmüller <business@academind.com>
2017-07-17 09:13:32 +0200

    updated to lastest vue + vuetify version

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
     </v-navigation-drawer>
     <v-toolbar dark class="primary">
       <v-toolbar-side-icon
-        @click.native.stop="sideNav = !sideNav"
+        @click.stop="sideNav = !sideNav"
         class="hidden-sm-and-up "></v-toolbar-side-icon>
       <v-toolbar-title>
         <router-link to="/" tag="span" style="cursor: pointer">DevMeetup</router-link>
@@ -27,7 +27,6 @@
           flat
           v-for="item in menuItems"
           :key="item.title"
-          router
           :to="item.link">
           <v-icon left dark>{{ item.icon }}</v-icon>
           {{ item.title }}
